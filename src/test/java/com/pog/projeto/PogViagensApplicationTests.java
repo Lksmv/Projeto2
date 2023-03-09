bf1e031908166fe56ed8f26e374e701aac342814
LucasMiguel <lks.migueel@gmail.com>
2023-03-09 17:08:25 -0300

    spring start

---
+++ src/test/java/com/pog/projeto/PogViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pog.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PogViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
