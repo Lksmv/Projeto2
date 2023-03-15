296cf0daa5faa81770aff6cc625645e89d6ba335
LucasMiguel <lks.migueel@gmail.com>
2023-03-15 15:24:38 -0300

    fix repository

--- src/main/java/com/pog/projeto/repository/PacoteRepository.java
+++ src/main/java/com/pog/projeto/repository/PacoteRepository.java
@@ -1,9 +1,10 @@
 package com.pog.projeto.repository;
 
 import com.pog.projeto.entity.HotelEntity;
+import com.pog.projeto.entity.PacoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PacoteRepository extends JpaRepository<HotelEntity, Integer> {
+public interface PacoteRepository extends JpaRepository<PacoteEntity, Integer> {
 }
--- src/main/java/com/pog/projeto/repository/PessoaRepository.java
+++ src/main/java/com/pog/projeto/repository/PessoaRepository.java
@@ -4,6 +4,10 @@ import com.pog.projeto.entity.PessoaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<PessoaEntity, Integer> {
+
+    public Optional<PessoaEntity> findPessoaEntityByEmail(String email);
 }
