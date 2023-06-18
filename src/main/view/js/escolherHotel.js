16bd8507d0476421f3b6dcabb1bb05858387ebe7
Bruno Reis <brunohenrique0805@hotmail.com>
2023-06-18 15:11:03 -0300

    ajuste fechar info window ao clicar em outro

--- src/main/view/js/escolherHotel.js
+++ src/main/view/js/escolherHotel.js
@@ -1,11 +1,14 @@
 let map;
 let markers = [];
+let infoWindowAtual = null;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("mapaHotel"), {
         center: { lat: -23.5505, lng: -46.6333 },
         zoom: 8,
     });
+
+    adicionarEventoChangeBotao();
 }
 
 function searchHotels() {
@@ -39,30 +42,39 @@ function searchHotels() {
                 bounds.extend(marker.getPosition());
 
                 google.maps.event.addListener(marker, 'click', function () {
-                    const service = new google.maps.places.PlacesService(document.createElement('div'));
-                    const request = {
-                        placeId: marker.hotelId,
-                        fields: ['name', 'price_level', 'rating', 'photos']
-                    };
+                    if (!document.getElementById('checkboxHotel').checked) {
+                        const service = new google.maps.places.PlacesService(document.createElement('div'));
+                        const request = {
+                            placeId: marker.hotelId,
+                            fields: ['name', 'price_level', 'rating', 'photos']
+                        };
 
-                    service.getDetails(request, function (place, status) {
-                        if (status === google.maps.places.PlacesServiceStatus.OK) {
-                            const content = `<div style="background-color: #f2f2f2; padding: 10px;">
-                            <h3>${place.name}</h3>
-                            <p>Preço diária: R$${getValorHotelAleatorio().toLocaleString('pt-BR')}</p>
-                            <p>Avaliação: ${place.rating}</p>
-                            <img src="${place.photos[0].getUrl()}" alt="Imagem do hotel" style="max-width: 200px;" />
-                            </div>`;
+                        service.getDetails(request, function (place, status) {
+                            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                                if (infoWindowAtual) {
+                                    infoWindowAtual.close();
+                                }
+                                const content = `<div style="background-color: #f2f2f2; padding: 10px;">
+                                <h3>${place.name}</h3>
+                                <p>Preço diária: R$${getValorHotelAleatorio().toLocaleString('pt-BR')}</p>
+                                <p>Avaliação: ${place.rating}</p>
+                                <img src="${place.photos[0].getUrl()}" alt="Imagem do hotel" style="max-width: 200px;" />
+                                </div>`;
 
-                            const infoWindow = new google.maps.InfoWindow({
-                                content: content
-                            });
+                                const infoWindow = new google.maps.InfoWindow({
+                                    content: content
+                                });
 
-                            infoWindow.open(map, marker);
-                        } else {
-                            console.error(status);
-                        }
-                    });
+                                infoWindowAtual = infoWindow;
+
+                                infoWindow.open(map, marker);
+                            } else {
+                                console.error(status);
+                            }
+                        });
+                    } else {
+                        infoWindowAtual = null;
+                    }
                 });
             }
 
@@ -78,4 +90,13 @@ function getValorHotelAleatorio() {
     const max = 3000;
 
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function adicionarEventoChangeBotao() {
+    const checkbox = document.getElementById('checkboxHotel');
+    checkbox.addEventListener('click', () => {
+        if (infoWindowAtual) {
+            infoWindowAtual.close();
+        }
+    });
 }
